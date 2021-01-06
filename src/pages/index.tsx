b7b22f4a110bb99de71ccf054fe12c3e564c5ab3
Rafael Rondao <rafael.rondao@gmail.com>
2021-01-06 20:48:45 -0300

    Create basic navigation bar
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,33 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Quantum</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <header id="navBar" className="py-6 border-b-2">
+        <div className="container flex justify-between mx-auto">
+          <a href="#">Quantum</a>
+          <ul className="flex">
+            <li className="px-2 mx-4">
+              <a href="#">Aulas Particulares</a>
+            </li>
+            <li className="px-2 mx-4">
+              <a href="#">Cursos</a>
+            </li>
+            <li className="px-2 mx-4">
+              <a href="#">Professores</a>
+            </li>
+            <li className="px-2 mx-4">
+              <a href="#">Entrar</a>
+            </li>
+          </ul>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </header>
     </div>
-  )
+  );
 }
