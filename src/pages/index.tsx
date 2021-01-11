ce3a8833b4169e9ecb11263c30dcf4548b3e8039
Rafael Rondao <rafael.rondao@gmail.com>
2021-01-11 20:04:23 -0300

    Add search for subject
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,29 @@ export default function Home() {
       </section>
 
       <section id="subjects" className="py-12 border-b-2">
-        <div className="container grid gap-8 mx-auto grid-cols-fill">
+        <div className="container flex w-1/2 p-2 mx-auto bg-gray-400 rounded-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white"
+            className="w-8 px-1"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />{" "}
+          </svg>
+          <input
+            type="text"
+            placeholder="Procurar por disciplina"
+            className="w-full placeholder-white bg-transparent border-b-2"
+          />
+        </div>
+        <div className="container grid gap-8 p-8 mx-auto grid-cols-fill">
           <div className="py-6 text-center bg-gray-400 rounded-lg">
             Disciplina 1
           </div>
